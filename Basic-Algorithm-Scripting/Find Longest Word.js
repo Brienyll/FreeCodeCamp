1c569b33da24db4270b1105fd22cdfa677c3e545
Brienyll <brienyll29@gmail.com>
2016-11-22 15:43:07 -0800

    Find the longest word on a string
    Return the length of the longest word in the provided sentence.
    
    Your response should be a number.

---
+++ Basic-Algorithm-Scripting/Find Longest Word.js
@@ -0,0 +1,16 @@
+function findLongestWord(str) {
+  //create array of strings breaking up every space
+  var arrayOfString = str.split(' ');
+  //temporary placeholder for the longest string
+  var longest = '';
+  // iterate through the array and compare the strings
+  for(i=0; i < arrayOfString.length; i++) {
+    if(longest.length < arrayOfString[i].length){
+      longest = arrayOfString[i];
+    }
+      
+  }
+  str = longest;
+  
+  return str.length;
+}
