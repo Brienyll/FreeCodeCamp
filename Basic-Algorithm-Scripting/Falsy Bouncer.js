34f7c422ac96f4d93aca8dd28fdddfdf57bc14b6
Brienyll <brienyll29@gmail.com>
2016-11-29 11:53:18 -0800

    created new solution
    changed function to boolean

--- Basic-Algorithm-Scripting/Falsy Bouncer.js
+++ Basic-Algorithm-Scripting/Falsy Bouncer.js
@@ -6,8 +6,6 @@ Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
 
 function bouncer(arr){
   // Don't show a false ID to this bouncer.
-   return arr.filter(function(item){
-     return item;
-    });
+   return arr.filter(Boolean)
 }
 bouncer([7, "ate", "", false, 9]);
