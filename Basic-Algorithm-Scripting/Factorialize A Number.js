d824cc4cb494421e72619dec2914eebc32f202a5
Brienyll <brienyll29@gmail.com>
2016-12-05 14:29:05 -0800

    changed solution
    and added comments

--- Basic-Algorithm-Scripting/Factorialize A Number.js
+++ Basic-Algorithm-Scripting/Factorialize A Number.js
@@ -1,9 +1,18 @@
-function factorialize(num) {
-  if (num < 2) {
-    return 1;
-  }
-  return num * factorialize(num - 1);
+/*
+Return the factorial of the provided integer.
 
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+*/
+
+function factorialize(num) {
+  var cnt =1;
+  
+  for (i=1; i<=num; i++){
+    cnt *= i;
+  }
+  return cnt;
 }
 
 factorialize(5);
