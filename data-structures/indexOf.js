5ff91afc2a5286584e017fadb0b0a5b47ece849b
Brix <brienyll29@gmail.com>
2019-06-18 09:32:11 -0700

    Create indexOf.js

---
+++ data-structures/indexOf.js
@@ -0,0 +1,13 @@
+function quickCheck(arr, elem) {
+
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+// false
